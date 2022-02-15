220d3b098f70db710d346dc268c6899c637c67a4
Kévin Marais <kevin.mrs2020@gmail.com>
2022-02-16 00:18:27 +0100

    Install AngularFire

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,6 +4,9 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { initializeApp,provideFirebaseApp } from '@angular/fire/app';
+import { environment } from '../environments/environment';
+import { provideFirestore,getFirestore } from '@angular/fire/firestore';
 
 @NgModule({
   declarations: [
@@ -12,7 +15,9 @@ import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
   imports: [
     BrowserModule,
     AppRoutingModule,
-    NgbModule
+    NgbModule,
+    provideFirebaseApp(() => initializeApp(environment.firebase)),
+    provideFirestore(() => getFirestore())
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,6 +3,15 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
+  firebase: {
+    projectId: 'crud-books-e71ea',
+    appId: '1:787394578789:web:76f6d664d49542346a798c',
+    storageBucket: 'crud-books-e71ea.appspot.com',
+    locationId: 'europe-west',
+    apiKey: 'AIzaSyD-w5dzHPNvm7I0mwgoZw-N0cItLORh9Dc',
+    authDomain: 'crud-books-e71ea.firebaseapp.com',
+    messagingSenderId: '787394578789',
+  },
   production: false,
   firebaseConfig : {
     apiKey: "AIzaSyD-w5dzHPNvm7I0mwgoZw-N0cItLORh9Dc",
