e258f2d0e4fdc0ea1d039b57b292ad55f3bd5493
Kévin Marais <kevin.mrs2020@gmail.com>
2022-02-16 00:28:16 +0100

    Create components, modal and service

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,19 +3,32 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 import { initializeApp,provideFirebaseApp } from '@angular/fire/app';
 import { environment } from '../environments/environment';
 import { provideFirestore,getFirestore } from '@angular/fire/firestore';
+import { FormsModule } from '@angular/forms';
+import { HomeComponent } from './components/home/home.component';
+import { BookComponent } from './components/book/book.component';
+import { BooksComponent } from './components/books/books.component';
+import { SearchComponent } from './components/search/search.component';
+import { EditBookComponent } from './modal/edit-book/edit-book.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    BookComponent,
+    BooksComponent,
+    SearchComponent,
+    EditBookComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     NgbModule,
+    FormsModule ,
     provideFirebaseApp(() => initializeApp(environment.firebase)),
     provideFirestore(() => getFirestore())
   ],
