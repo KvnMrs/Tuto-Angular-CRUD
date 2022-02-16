bd8c1be4da953fb59f38d9b0401cdc78e099e7fa
Kévin Marais <kevin.mrs2020@gmail.com>
2022-02-16 13:23:32 +0100

    feat : deleteBook service

--- src/app/services/book.service.ts
+++ src/app/services/book.service.ts
@@ -1,6 +1,6 @@
 import { Injectable } from '@angular/core';
 import {
-  Firestore, addDoc, collection, collectionData
+  Firestore, addDoc, collection, collectionData, doc, deleteDoc
 } from '@angular/fire/firestore';
 import { Observable } from 'rxjs';
 import { IBook } from '../models/book.model';
@@ -21,4 +21,9 @@ export class BookService {
     const booksRef = collection(this.firestore, 'books');
     return collectionData(booksRef, { idField: 'id' }) as Observable<IBook[]>;
   }
+
+  deleteBook(book: IBook) {
+    const bookDocRef = doc(this.firestore, `books/${book.id}`);
+    return deleteDoc(bookDocRef);
+}
 }
