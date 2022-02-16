1bec5365fa832ea77ef3b09d0447f2233fd331cb
Kévin Marais <kevin.mrs2020@gmail.com>
2022-02-16 10:18:51 +0100

    feat : addBook service

--- src/app/services/book.service.ts
+++ src/app/services/book.service.ts
@@ -1,9 +1,18 @@
 import { Injectable } from '@angular/core';
+import {
+  Firestore, addDoc, collection
+} from '@angular/fire/firestore';
+import { IBook } from '../models/book.model';
 
 @Injectable({
   providedIn: 'root'
 })
 export class BookService {
 
-  constructor() { }
+  constructor(private firestore: Firestore) { }
+  
+  addBook(book: IBook) {
+    const booksRef = collection(this.firestore, 'books'); 
+    return addDoc(booksRef, book);
+  }
 }
