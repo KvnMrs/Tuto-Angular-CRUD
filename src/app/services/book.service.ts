a70966ff12ad4b31f3f979d8591292495c463546
Kévin Marais <kevin.mrs2020@gmail.com>
2022-02-16 13:36:51 +0100

    feat : getBookByID service, updateBook service

--- src/app/services/book.service.ts
+++ src/app/services/book.service.ts
@@ -1,6 +1,6 @@
 import { Injectable } from '@angular/core';
 import {
-  Firestore, addDoc, collection, collectionData, doc, deleteDoc
+  Firestore, addDoc, collection, collectionData, doc, deleteDoc, docData, setDoc, updateDoc
 } from '@angular/fire/firestore';
 import { Observable } from 'rxjs';
 import { IBook } from '../models/book.model';
@@ -22,8 +22,25 @@ export class BookService {
     return collectionData(booksRef, { idField: 'id' }) as Observable<IBook[]>;
   }
 
+  getBookByID(id: string) {
+    const bookRef = doc(this.firestore, `books/${id}`);
+    return docData(bookRef, { idField: 'id' }) as Observable<IBook>;
+  }
+
   deleteBook(book: IBook) {
     const bookDocRef = doc(this.firestore, `books/${book.id}`);
     return deleteDoc(bookDocRef);
+}
+
+
+
+updateBook(book: IBook) {
+  const bookDocRef = doc(this.firestore, `books/${book.id}`);
+  return setDoc(bookDocRef, book);
+}
+
+modifyBookPrice(book: IBook, amount: number) {
+  const bookDocRef = doc(this.firestore, `books/${book.id}`);
+  return updateDoc(bookDocRef, { price: amount });
 }
 }
